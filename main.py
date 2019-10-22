6136c46a74dab6dfda5674d594ed472d8eab4e55
krnick <sungboss2004@gmail.com>
2019-10-22 09:53:03 +0800

    method search

--- main.py
+++ main.py
@@ -15,8 +15,19 @@ class x_rule:
     def permissions(self):
         return self.a.get_permissions()
 
+    def methods(self, method_name):
+        # check lenght
+        result = self.dx.find_methods(methodname=method_name)
+        if len(list(result)) > 0:
+            return result
+        else:
+            raise ValueError('No Method Name Found:'+method_name)
+
+
 
 data = x_rule('14d9f1a92dd984d6040cc41ed06e273e.apk')
 
 
-print(data.permissions)
+#print(data.permissions)
+for i in data.methods('sendTextMessge'):
+    print(i)
