e049d2a536479a6b43db8b74c0e3ee58b98a5e9a
krnick <sungboss2004@gmail.com>
2019-10-22 11:32:51 +0800

    up to rule 3

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+import json
 from androguard.core.bytecodes import dvm
 from androguard.core.analysis import analysis
 from androguard.misc import AnalyzeAPK, AnalyzeDex
@@ -11,18 +13,25 @@ class x_rule:
     def permissions(self):
         return self.a.get_permissions()
 
-    def methods(self, method_name):
-        # check lenght
-        result = self.dx.find_methods(methodname=method_name)
-        if len(list(result)) > 0:
-            return result
-        else:
-            raise ValueError("No Method Name Found:" + method_name)
+    def methods(self, class_name=".*", method_name=".*"):
+
+        self.dx.create_xref()
+
+        return self.dx.find_methods(classname=class_name, methodname=method_name)
 
 
 data = x_rule("14d9f1a92dd984d6040cc41ed06e273e.apk")
 
+with open("sendSMS.json", "r") as f:
+    jl = json.loads(f.read())
 
-# print(data.permissions)
-for i in data.methods("sendTextMessge"):
-    print(i)
+    if jl["x1_permission"] in data.permissions:
+        print("有權限:" + jl["x1_permission"])
+
+    if len(list(data.methods(method_name=jl["x2_method"]))) > 0:
+        print("有Method:" + jl["x2_method"])
+
+    if len(
+        list(data.methods("Landroid/telephony/SmsManager", jl["x3_combination"][0]))
+    ):
+        print("有Method:" + jl["x3_combination"][0])
