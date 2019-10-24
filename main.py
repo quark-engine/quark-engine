6121563e63bcf8ed8761998ff7120ca0aa611cd0
krnick <sungboss2004@gmail.com>
2019-10-24 10:39:06 +0800

    refactor

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from RuleObject import RuleObject
 from androguard.core.bytecodes import dvm
 from androguard.core.analysis import analysis
 from androguard.misc import AnalyzeAPK, AnalyzeDex
+import operator
 
 # TODO  whenever use the list(), need to check it is NoneType or not
 
@@ -10,16 +11,19 @@ class x_rule:
     def __init__(self, apk):
         self.a, self.d, self.dx = AnalyzeAPK(apk)
 
-        # Create Class, Method, String and Field crossreferences
-        # for all classes in the Analysis.
+        # Create Class, Method, String and Field
+        # crossreferences for all classes in the Analysis.
         self.dx.create_xref()
+
+        self.pre_method0 = []
+        self.pre_method1 = []
 
     @property
     def permissions(self):
 
         return self.a.get_permissions()
 
-    def methods(self, class_name=".*", method_name=".*"):
+    def find_method(self, class_name=".*", method_name=".*"):
 
         result = self.dx.find_methods(class_name, method_name)
 
@@ -30,8 +34,12 @@ class x_rule:
             return None
 
     def upperFunc(self, class_name, method_name):
+        """
+        Find the upper level method from given method.
+        """
+
         result = []
-        method_set = self.methods(class_name, method_name)
+        method_set = self.find_method(class_name, method_name)
 
         if method_set is not None:
             for md in method_set:
@@ -43,35 +51,84 @@ class x_rule:
             return None
 
     def _remove_dup(self, element):
+        """
+        Remove the duplicate elements in  given list.
+        """
         return list(set(element))
 
     def find_intersection(self, list1, list2, depth=1):
+        """
+        Find the list1 ∩ list2.
+        """
+        # Check the list is not null
+        if len(list1) > 0 and len(list2) > 0:
 
-        # Limit up to three layers of the recursions.
-        if depth == 3:
-            return None
+            # Limit up to three layers of the recursions.
+            if depth == 3:
+                return None
 
-        result = set(list1).intersection(list2)
-        if len(result) > 0:
-            return result
+            result = set(list1).intersection(list2)
+            if len(result) > 0:
+
+                return result
+            else:
+                # Not found same method usage, try to find the next layer.
+
+                next_list1 = []
+                next_list2 = []
+                for item in list1:
+                    next_list1 = self.upperFunc(".*", item)
+                for item in list2:
+                    next_list2 = self.upperFunc(".*", item)
+                # Append first layer into next layer
+                for pre_list in list1:
+                    next_list1.append(pre_list)
+                for pre_list in list2:
+                    next_list2.append(pre_list)
+
+                depth += 1
+
+                # To find the same method, push the previous two
+                # methods into the stack here. Once it found there
+                # is same method, pop the previous method from stack.
+                self.pre_method0.append(list1)
+                self.pre_method1.append(list2)
+
+                return self.find_intersection(next_list1, next_list2, depth)
         else:
-            # Not found same method usage, try to find next layer.
+            raise ValueError("List is Null")
 
-            sec_list1 = []
-            sec_list2 = []
-            for item in list1:
-                sec_list1 = self.upperFunc(".*", item)
-            for item in list2:
-                sec_list2 = self.upperFunc(".*", item)
-            # Append first layer into next layer
-            for pre_list in list1:
-                sec_list1.append(pre_list)
-            for pre_list in list2:
-                sec_list2.append(pre_list)
+    def check_sequence(self, class_name, method_name, first_func, second_func):
+        method_set = self.find_method(class_name, method_name)
+        seq_table = []
 
-            depth += 1
+        if method_set is not None:
+            for md in method_set:
+                for _, call, number in md.get_xref_to():
+                    if call.name == first_func or call.name == second_func:
+                        seq_table.append((call.name, number))
+            # sorting based on the value of the number
+            seq_table.sort(key=operator.itemgetter(1))
 
-            return self.find_intersection(sec_list1, sec_list2, depth)
+            idx = 0
+            length = len(seq_table)
+            first_firstfunc_val = None
+            first_secondfunc_val = None
+            while idx < length:
+                if seq_table[idx][0] == first_func:
+                    first_firstfunc_val = idx
+                    break
+                idx += 1
+            while length > 0:
+                if seq_table[length - 1][0] == second_func:
+                    first_secondfunc_val = length - 1
+                    break
+                length -= 1
+
+            if first_secondfunc_val > first_firstfunc_val:
+                return True
+            else:
+                return False
 
 
 data = x_rule("14d9f1a92dd984d6040cc41ed06e273e.apk")
@@ -82,20 +139,20 @@ rule_checker = RuleObject("sendLocation.json")
 
 # Level 1
 if set(rule_checker.x1_permission).issubset(set(data.permissions)):
-    print("[O]有使用權限: \n" + ",".join(rule_checker.x1_permission))
+    print("1==> [O]")
 
 # Level 2
 test_md0 = rule_checker.x2n3n4_comb[0]["method"]
 test_cls0 = rule_checker.x2n3n4_comb[0]["class"]
-if data.methods(test_cls0, test_md0) is not None:
-    print("[O]有使用method: \n" + test_md0)
+if data.find_method(test_cls0, test_md0) is not None:
+    print("2==> [O]")
 
     test_md1 = rule_checker.x2n3n4_comb[1]["method"]
     test_cls1 = rule_checker.x2n3n4_comb[1]["class"]
 
     # Level 3
-    if data.methods(test_cls1, test_md1) is not None:
-        print("[O]也有使用method: \n" + test_md1)
+    if data.find_method(test_cls1, test_md1) is not None:
+        print("3==> [O]")
 
     # Level 4
     upperfunc0 = data.upperFunc(test_cls0, test_md0)
@@ -103,4 +160,11 @@ if data.methods(test_cls0, test_md0) is not None:
 
     same = data.find_intersection(upperfunc0, upperfunc1)
     if same is not None:
-        print("[O]共同出現於:\n" + repr(same))
+
+        # print("[O]共同出現於:\n" + repr(same))
+        pre_0 = data.pre_method0.pop()[0]
+        pre_1 = data.pre_method1.pop()[0]
+
+        if data.check_sequence(".*", "sendMessage", pre_0, pre_1):
+            print("4==> [O]")
+
