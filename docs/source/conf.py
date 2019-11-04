8110142e0cda328eb5fc87023bc316d4d050cc5f
KunYuChen <xspiritualx@gmail.com>
2019-11-04 10:06:42 +0800

    add configuration for doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,3 +52,4 @@ html_theme = 'sphinx_rtd_theme' #'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+master_doc = 'index'
