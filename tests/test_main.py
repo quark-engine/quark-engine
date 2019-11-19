0d45b56ea767af78054ba7a122e95650e6642efb
sidra-asa <sasakikung@gmail.com>
2019-11-19 15:15:51 +0800

    Using pytest instead of unittest in test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,20 @@
 
-import unittest
+import pytest
 from androguard.core.analysis.analysis import MethodAnalysis
 
 from quark.main import XRule
 
 
-class TestXRule(unittest.TestCase):
+@pytest.fixture()
+def xrule_obj(scope="fuction"):
+    apk_file = "quark/sample/13667fe3b0ad496a0cd157f34b7e0c991d72a4db.apk"
+    xrule_obj = XRule(apk_file)
+    yield xrule_obj
 
-    def setUp(self):
-        apk_file = "quark/sample/13667fe3b0ad496a0cd157f34b7e0c991d72a4db.apk"
-        self.xrule_obj = XRule(apk_file)
 
-    def test_permissions(self):
+class TestXRule():
+
+    def test_permissions(self, xrule_obj):
         ans = [
             'android.permission.SEND_SMS',
             'android.permission.RECEIVE_BOOT_COMPLETED',
@@ -22,18 +25,17 @@ class TestXRule(unittest.TestCase):
             'android.permission.RECEIVE_SMS',
             'android.permission.READ_CONTACTS'
         ]
+        assert set(xrule_obj.permissions) == set(ans)
 
-        self.assertEqual(set(self.xrule_obj.permissions), set(ans))
+    def test_find_method(self, xrule_obj):
+        result = list(xrule_obj.find_method("Ljava/lang/reflect/Field"))
+        assert len(result) == 2
+        assert isinstance(result[0], MethodAnalysis)
 
-    def test_find_method(self):
-        result = list(self.xrule_obj.find_method("Ljava/lang/reflect/Field"))
-        self.assertEqual(len(result), 2)
-        self.assertIsInstance(result[0], MethodAnalysis)
+    def test_upperFunc(self, xrule_obj):
+        result = xrule_obj.upperFunc("Ljava/lang/reflect/Field",
+                                     "setAccessible")
+        expect_func = "Landroid/support/v4/widget/SlidingPaneLayout$"\
+                      "SlidingPanelLayoutImplJB;"
+        assert expect_func in result[0]
 
-    def test_upperFunc(self):
-        result = self.xrule_obj.upperFunc("Ljava/lang/reflect/Field", "setAccessible")
-        self.assertIn("Landroid/support/v4/widget/SlidingPaneLayout$SlidingPanelLayoutImplJB;", result[0])
-
-
-if __name__ == '__main__':
-    unittest.main()
