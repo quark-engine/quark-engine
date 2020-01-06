61a8e762366a5178e7c66e3e5f0aa8b238816fef
krnick <sungboss2004@gmail.com>
2020-01-06 14:12:47 +0800

    add test for find_intersection

--- tests/Object/test_xrule.py
+++ tests/Object/test_xrule.py
@@ -22,3 +22,38 @@ class TestXRule():
         xrule.find_previous_method(base_method, top_method, test_list)
 
         assert test_list == expected_list
+
+    def test_find_intersection(self, xrule):
+        location_api_upper = [("Lcom/google/progress/Locate;", "getLocation")]
+        sms_api_upper = [("Lcom/google/progress/SMSHelper;", "sendSms")]
+        contact_api_upper = [("Lcom/google/progress/APNOperator;", "addAPN"),
+                             ("Lcom/google/progress/ContactsCollecter;", "getPhoneNumbers"),
+                             ("Lcom/google/progress/APNOperator;", "getAPNList"),
+                             ("Lcom/google/progress/SMSHelper;", "readSMSList"),
+                             ("Lcom/google/progress/ContactsCollecter;", "getEmail"),
+                             ("Lcom/google/progress/APNOperator;", "checkAPNisAvailable"),
+                             ("Lcom/google/progress/APNOperator;, deleteAPN"),
+                             ("Lcom/google/progress/ContactsCollecter;", "getContactList"),
+                             ("Lcom/google/progress/GetCallLog;", "getCallLog")]
+
+        with pytest.raises(ValueError, match="List is Null"):
+            xrule.find_intersection([], [])
+            xrule.find_intersection([], [1])
+            xrule.find_intersection([1], [])
+
+        assert len(set(location_api_upper).intersection(sms_api_upper)) == 0
+
+        # When there is no intersection in first layer, it will try to enter the second layer to check the intersection.
+
+        # Send Location via SMS
+        expected_result_location = {("Lcom/google/progress/AndroidClientService;", "doByte"),
+                                    ("Lcom/google/progress/AndroidClientService;", "sendMessage"),
+                                    ("Lcom/google/progress/AndroidClientService$2;", "run")}
+
+        # Send contact via SMS
+        expected_result_contact = {("Lcom/google/progress/AndroidClientService;", "doByte"),
+                                   ("Lcom/google/progress/AndroidClientService;", "sendMessage")}
+
+        assert xrule.find_intersection(location_api_upper, sms_api_upper) == expected_result_location
+
+        assert xrule.find_intersection(contact_api_upper, sms_api_upper) == expected_result_contact
