a624325d20614ff205f6d663a8527ae46e1be383
krnick <sungboss2004@gmail.com>
2020-01-07 10:16:40 +0800

    add function description, and follow sphinx style

--- quark/Objects/BytecodeObject.py
+++ quark/Objects/BytecodeObject.py
@@ -1,5 +1,12 @@
 class BytecodeObject:
     def __init__(self, mnemonic, registers, parameter):
+        """
+        ['invoke-virtual', 'v3', 'Lcom/google/progress/APNOperator;->deleteAPN()Z']
+
+        :param mnemonic:
+        :param registers:
+        :param parameter:
+        """
         self._mnemonic = mnemonic
         self._registers = registers
         self._parameter = parameter
@@ -10,12 +17,28 @@ class BytecodeObject:
 
     @property
     def mnemonic(self):
+        """
+        Dalvik bytecode instructions set, for example 'invoke-virtual'.
+
+        :return: a string of mnemonic
+        """
         return self._mnemonic
 
     @property
     def registers(self):
+        """
+        Registers used in Dalvik instructions, for example '[v3]'.
+
+        :return: a list containing all the registers used
+        """
         return self._registers
 
     @property
     def parameter(self):
+        """
+        Commonly used for functions called by invoke-kind instructions, for example
+        'Lcom/google/progress/APNOperator;->deleteAPN()Z'.
+
+        :return: a string of the function name
+        """
         return self._parameter
