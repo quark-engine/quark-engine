e441f8fbd841578f33025e98fa7f944cc6c2b474
krnick <sungboss2004@gmail.com>
2020-01-30 15:20:14 +0800

    Add quark logo to read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,5 +52,6 @@ html_theme = 'sphinx_rtd_theme'  # 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 master_doc = 'index'
+html_logo = "quark-logo.png"
