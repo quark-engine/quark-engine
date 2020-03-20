d2783684fe463c6b6f101cde22fae034b9260a8a
JunWei Song <sungboss2004@gmail.com>
2020-03-20 09:51:27 +0800

    Bugfix for issue #46 (#49)
    * Xrule bugfix (#48)
    
    Co-authored-by: JunWei Song <sungboss2004@gmail.com>
    
    Co-authored-by: sidra-asa <sasakikung@gmail.com>

--- quark/Objects/xrule.py
+++ quark/Objects/xrule.py
@@ -45,7 +45,7 @@ class XRule:
         # Sum of the each rule
         self.score_sum = 0
 
-    def find_previous_method(self, base_method, top_method, pre_method_list):
+    def find_previous_method(self, base_method, top_method, pre_method_list, visited_methods=None):
         """
         Find the previous method based on base method before top method.
         This will append the method into pre_method_list.
@@ -53,10 +53,15 @@ class XRule:
         :param base_method: the base function which needs to be searched.
         :param top_method: the top-level function which calls the basic function.
         :param pre_method_list: list is used to track each function.
+        :param visited_methods: set with tested method.
         :return: None
         """
+        if visited_methods is None:
+            visited_methods = set()
+
         class_name, method_name = base_method
         method_set = self.apkinfo.upperfunc(class_name, method_name)
+        visited_methods.add(base_method)
 
         if method_set is not None:
 
@@ -64,11 +69,11 @@ class XRule:
                 pre_method_list.append(base_method)
             else:
                 for item in method_set:
-                    # prevent some functions from looking for themselves.
-                    if item == base_method:
+                    # prevent to test the tested methods.
+                    if item in visited_methods:
                         continue
                     self.find_previous_method(
-                        item, top_method, pre_method_list)
+                        item, top_method, pre_method_list, visited_methods)
 
     def find_intersection(self, list1, list2, depth=1):
         """
