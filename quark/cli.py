9a73f19e17051e3d6f95020dfc07d2d0947443e7
Shiang <pulorsok@gmail.com>
2020-08-03 15:06:21 +0800

    add click option for json report

--- quark/cli.py
+++ quark/cli.py
@@ -1,22 +1,26 @@
-import os
-import json
-
 import click
-from tqdm import tqdm
-
+import json as json_module
+import os
 from quark.Objects.ruleobject import RuleObject
-from quark.logo import logo
 from quark.Objects.xrule import XRule
+from quark.logo import logo
 from quark.utils.out import print_success, print_info, print_warning
+from quark.utils.tools import write_json_report, hash_apk
 from quark.utils.weight import Weight
+from tqdm import tqdm
 
 logo()
 
 
 @click.command()
-@click.option("--jreport", "-j", is_flag=True, help='show json report')
+@click.option("--json", "-j", is_flag=True, help='show json report')
 @click.option("--summary", "-s", is_flag=True, help='show summary report')
 @click.option("--detail", "-d", is_flag=True, help="show detail report")
+@click.option(
+    "-o", "--output", help="Output report as json file",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False),
+    required=False,
+)
 @click.option(
     "-a", "--apk", help="APK file", type=click.Path(exists=True, file_okay=True, dir_okay=False),
     required=True,
@@ -25,9 +29,9 @@ logo()
     "-r", "--rule", help="Rules folder need to be checked",
     type=click.Path(exists=True, file_okay=False, dir_okay=True), required=True,
 )
-def entry_point(summary, detail, jreport, apk, rule):
+def entry_point(summary, detail, json, apk, rule, output):
     """Quark is an Obfuscation-Neglect Android Malware Scoring System"""
-      
+
     if summary:
         # show summary report
         # Load APK
@@ -69,10 +73,10 @@ def entry_point(summary, detail, jreport, apk, rule):
 
             data.show_detail_report(rule_checker)
             print_success("OK")
-    
-    if jreport:
+
+    if json:
         # show json report
-        
+
         # Load APK
         data = XRule(apk)
 
@@ -86,12 +90,24 @@ def entry_point(summary, detail, jreport, apk, rule):
             # Run the checker
             data.run(rule_checker)
 
-            data.show_json_report(rule_checker, False)
+            data.show_json_report(rule_checker)
 
         w = Weight(data.score_sum, data.weight_sum)
         print_warning(w.calculate())
         print_info("Total Score: " + str(data.score_sum))
-        print(json.dumps(data.json_report, indent=4))
+        print(json_module.dumps(data.json_report, indent=4))
+
+        # Output report as json file
+        if output is not None:
+            json_report = {
+                "sample": hash_apk(apk),
+                "apk-name": os.path.basename(apk),
+                "size": os.path.getsize(apk),
+                "warnning": w.calculate(),
+                "total-score": data.score_sum,
+                "crimes": data.json_report,
+            }
+            write_json_report(output, json_report)
 
 
 if __name__ == '__main__':
