82ac9d23b72c387cf7ed4a361462882c0b9b6e4d
krnick <sungboss2004@gmail.com>
2021-01-05 14:50:50 +0800

    Add feature of checking for updates for quark-rules

--- quark/cli.py
+++ quark/cli.py
@@ -6,11 +6,13 @@ from tqdm import tqdm
 
 from quark.Objects.quark import Quark
 from quark.Objects.quarkrule import QuarkRule
+from quark.freshquark import check_update
 from quark.logo import logo
 from quark.utils.out import print_success, print_info, print_warning
 from quark.utils.weight import Weight
 
 logo()
+check_update()
 
 
 @click.command(no_args_is_help=True)
