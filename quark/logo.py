e2a86e8a93e072f93eb1da54cb66092f54fff716
krnick <sungboss2004@gmail.com>
2021-01-19 11:12:15 +0800

    refactor and add license to logo file

--- quark/logo.py
+++ quark/logo.py
@@ -1,9 +1,18 @@
+# -*- coding: utf-8 -*-
+# This file is part of Quark-Engine - https://github.com/quark-engine/quark-engine
+# See the file 'LICENSE' for copying permission.
+
 from quark import __version__
 
 from quark.utils.colors import bold, lightyellow, lightblue
 
 
 def logo():
+    """
+    Output our amazing logo
+
+    :return: None
+    """
     print(
         bold(
             lightyellow(
@@ -20,8 +29,8 @@ def logo():
         + bold(
             lightblue(
                 """
-            An Obfuscation-Neglect Android Malware Scoring System
-            """
+                An Obfuscation-Neglect Android Malware Scoring System
+                """
             ),
         ),
     )
