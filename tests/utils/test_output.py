e5d8c64cfc64e7f597748d8e6942835114b71d38
krnick <sungboss2004@gmail.com>
2021-02-18 21:28:45 +0800

    reformatted with black

--- tests/utils/test_output.py
+++ tests/utils/test_output.py
@@ -4,8 +4,10 @@ import requests
 from quark.utils.output import output_parent_function_table
 from quark.Objects.quark import Quark
 
-APK_SOURCE = "https://github.com/quark-engine/apk-malware-samples" \
-             "/raw/master/14d9f1a92dd984d6040cc41ed06e273e.apk"
+APK_SOURCE = (
+    "https://github.com/quark-engine/apk-malware-samples"
+    "/raw/master/14d9f1a92dd984d6040cc41ed06e273e.apk"
+)
 APK_FILENAME = "14d9f1a92dd984d6040cc41ed06e273e.apk"
 
 
@@ -19,33 +21,27 @@ def quark_obj(scope="function"):
 
 def test_output_parent_function_table(capsys, quark_obj):
     method_obj = quark_obj.apkinfo.find_method(
-        'Lcom/google/progress/WifiCheckTask;', 'goConnectNetwork', '()V')
+        "Lcom/google/progress/WifiCheckTask;", "goConnectNetwork", "()V"
+    )
 
-    call_graph_analysis_list = ({
-        'crime': 'The Crime',
-        'parent': method_obj
-    }, {
-        'crime': 'The Crime',
-        'parent': method_obj
-    }, {
-        'crime': 'Another Crime',
-        'parent': method_obj
-    })
+    call_graph_analysis_list = (
+        {"crime": "The Crime", "parent": method_obj},
+        {"crime": "The Crime", "parent": method_obj},
+        {"crime": "Another Crime", "parent": method_obj},
+    )
 
     # Test one crime
     output_parent_function_table([call_graph_analysis_list[0]])
 
     output = capsys.readouterr().out
-    assert output.count(
-        'Lcom/google/progress/WifiCheckTask;goConnectNetwork') == 1
-    assert output.count('The Crime') == 1
+    assert output.count("Lcom/google/progress/WifiCheckTask;goConnectNetwork") == 1
+    assert output.count("The Crime") == 1
 
     # Test crimes with duplicated description
     output_parent_function_table(call_graph_analysis_list)
 
     output = capsys.readouterr().out
     print(output)
-    assert output.count(
-        'Lcom/google/progress/WifiCheckTask;goConnectNetwork') == 1
-    assert output.count('The Crime') == 1
-    assert output.count('Another Crime') == 1
+    assert output.count("Lcom/google/progress/WifiCheckTask;goConnectNetwork") == 1
+    assert output.count("The Crime") == 1
+    assert output.count("Another Crime") == 1
