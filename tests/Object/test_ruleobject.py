32761600aa2e6b0074ee77d770f2c8b74cb3c876
haeter525 <cindejze412@gmail.com>
2021-07-08 18:18:10 +0800

    Merge test_quarkrule.py and test_ruleobject.py

--- tests/Object/test_ruleobject.py
+++ tests/Object/test_ruleobject.py
@@ -1,8 +1,7 @@
 import os
 
 import pytest
-
-from quark.Objects.quarkrule import QuarkRule
+from quark.Objects.ruleobject import RuleObject
 
 
 @pytest.fixture()
@@ -38,7 +37,7 @@ def rule_obj(scope="function"):
 
     print("setup() begin")
 
-    rule_obj = QuarkRule("sendLocation.json")
+    rule_obj = RuleObject("sendLocation.json")
 
     yield rule_obj
 
@@ -46,29 +45,73 @@ def rule_obj(scope="function"):
     os.remove("sendLocation.json")
 
 
-class TestRuleObject:
-    def test_init(self, rule_obj):
-        assert rule_obj.crime == "Send Location via SMS"
+@pytest.fixture(scope="function")
+def invalid_file(tmp_path):
+    invalid_file = tmp_path / "invalid_file.txt"
+    invalid_file.write_text("Not a json")
 
-        expected_permissions = [
+    yield invalid_file
+
+
+@pytest.fixture(scope="function")
+def incomplete_rule(tmp_path):
+    incomplete_rule = tmp_path / "incomplete_rule.txt"
+    incomplete_rule.write_text("{}")
+
+    yield incomplete_rule
+
+
+@pytest.fixture(scope="function")
+def complete_rule():
+    return "quark/rules/sendLocation_SMS.json"
+
+
+class TestRuleObject:
+    def test_init_with_invalid_path(self):
+        with pytest.raises(TypeError):
+            _ = RuleObject(["Not", "a", "file"])
+
+    def test_init_with_non_exist_file(self):
+        with pytest.raises(FileNotFoundError):
+            _ = RuleObject("NON_EXIST_FILE")
+
+    def test_init_with_invalid_file(self, invalid_file):
+        with pytest.raises(BaseException):
+            _ = RuleObject(invalid_file)
+
+    def test_init_with_incomplete_rule(self, incomplete_rule):
+        with pytest.raises(KeyError):
+            _ = RuleObject(incomplete_rule)
+
+    def test_init_with_complete_rule(self, complete_rule):
+        rule = RuleObject(complete_rule)
+
+        assert all(rule.check_item) is False
+        assert rule.crime == "Send Location via SMS"
+        assert rule.permission == [
             "android.permission.SEND_SMS",
             "android.permission.ACCESS_COARSE_LOCATION",
             "android.permission.ACCESS_FINE_LOCATION",
         ]
-
-        assert rule_obj.permission == expected_permissions
-
-        expected_x2n3n4_comb = [
+        assert rule.api == [
             {
                 "class": "Landroid/telephony/TelephonyManager",
                 "method": "getCellLocation",
+                "descriptor": "()Landroid/telephony/CellLocation;",
             },
-            {"class": "Landroid/telephony/SmsManager", "method": "sendTextMessage"},
+            {
+                "class": "Landroid/telephony/SmsManager",
+                "method": "sendTextMessage",
+                "descriptor": (
+                    "(Ljava/lang/String; Ljava/lang/String;"
+                    " Ljava/lang/String; Landroid/app/PendingIntent;"
+                    " Landroid/app/PendingIntent;)V"
+                ),
+            },
         ]
-
-        assert rule_obj.api == expected_x2n3n4_comb
-
-        assert rule_obj.score == 4
+        assert rule.score == 4
+        assert rule.rule_filename == "sendLocation_SMS.json"
+        assert rule.label == ["location", "collection"]
 
     def test_get_score(self, rule_obj):
         confidence = [1, 2, 3, 4, 5]
