b5fbe80dcccdd2d762df5b004c364da0a3f126da
krnick <sungboss2004@gmail.com>
2021-08-24 13:35:49 +0800

    add permission

--- quark/core/parallelquark.py
+++ quark/core/parallelquark.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# This file is part of Quark-Engine - https://github.com/quark-engine/quark-engine
+# See the file 'LICENSE' for copying permission.
+
 from multiprocessing.pool import Pool
 
 from quark.core.analysis import QuarkAnalysis
