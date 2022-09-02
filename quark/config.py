2f6683b8fdad4746ebed8c4dfd9cbe85cf9c4ed2
haeter525 <cindejze412@gmail.com>
2022-09-02 17:56:05 +0800

    Set compatible rizin version to 0.3.4

--- quark/config.py
+++ quark/config.py
@@ -9,9 +9,9 @@ SOURCE = "https://github.com/quark-engine/quark-rules"
 DIR_PATH = f"{HOME_DIR}quark-rules"
 
 DEBUG = False
-COMPATIBLE_RAZIN_VERSIONS = ["0.4.0"]
+COMPATIBLE_RAZIN_VERSIONS = ["0.3.4"]
 
 RIZIN_DIR = f"{HOME_DIR}rizin/"
-RIZIN_COMMIT = "de8a5cac5532845643a52d1231b17a7b34feb50a"
+RIZIN_COMMIT = "2373c53880e017be86afed5454bca5b5017b920e"
 
 Path(HOME_DIR).mkdir(parents=True, exist_ok=True)
