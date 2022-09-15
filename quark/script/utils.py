cda85434f87c2873962f344154a4bb746087b9c2
haeter525 <cindejze412@gmail.com>
2022-09-15 14:13:03 +0800

    Rename `isTrue` as `isArgumentTrue`

--- quark/script/utils.py
+++ quark/script/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # This file is part of Quark-Engine - https://github.com/quark-engine/quark-engine
 # See the file 'LICENSE' for copying permission.
-def isTrue(argument: str) -> bool:
+def isArgumentTrue(argument: str) -> bool:
     """Check if the argument holds the Boolean value, True.
 
     :param argument: string that holds the value of a register
--- tests/script/test_utils.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# This file is part of Quark-Engine - https://github.com/quark-engine/quark-engine
-# See the file 'LICENSE' for copying permission.
-from quark.script.utils import isTrue
-
-
-def testIsTrue():
-    argument = "1"
-    assert isTrue(argument)
