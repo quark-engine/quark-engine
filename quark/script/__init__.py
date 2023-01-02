350b35c880193cae81d62e777a03ac5d9f164fe2
zinwang <chriswangxxxxx@gmail.com>
2023-01-03 01:55:02 +0800

    Fix a bug in API methodInstance.getArguments()

--- quark/script/__init__.py
+++ quark/script/__init__.py
@@ -152,7 +152,7 @@ class Method:
                 lambda record: methodPattern in record,
                 register_usage_records))
 
-            argumentStr = max(matchedRecords, key=len)[:-1]
+            argumentStr = max(matchedRecords, key=len, default="")[:-1]
             filterStr = f"{self.targetMethod.innerObj.class_name}->" + \
                 self.targetMethod.innerObj.name + \
                 self.targetMethod.descriptor
