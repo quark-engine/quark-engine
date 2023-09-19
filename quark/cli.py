2a4f543fdac9bad7e64ac5c08a4a659ba56b5418
sidra-asa <sasakikung@gmail.com>
2023-09-20 00:40:55 +0800

    Add radare2 as a choice of core_library

--- quark/cli.py
+++ quark/cli.py
@@ -133,7 +133,7 @@ logo()
     "--core-library",
     "core_library",
     help="Specify the core library used to analyze an APK",
-    type=click.Choice(("androguard", "rizin"), case_sensitive=False),
+    type=click.Choice(("androguard", "rizin", "radare2"), case_sensitive=False),
     required=False,
     default="androguard",
 )
