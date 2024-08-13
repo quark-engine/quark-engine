6fe83d2eacaf3f09f04b9028701520fbbf6fe29f
haeter525 <cindejze412@gmail.com>
2024-08-13 14:42:11 +0800

    Make the prompt clearer

--- quark/agent/prompts.py
+++ quark/agent/prompts.py
@@ -3,23 +3,23 @@
 # See the file 'LICENSE' for copying permission.
 
 SUMMARY_REPORT_FORMAT = """
-When prompted to provide a summary report, follow this format:
+When prompted to provide a summary report, follow these rules and the summary report example:
 
 	1. Print a newline character first to prevent formatting issues.
-	2. "\x1b[1m\x1b[33m[!]\x1b[0m\x1b[0m WARNING: High Risk” - Display the risk level with each word capitalized.
-	3. “\x1b[1m\x1b[36m[*]\x1b[0m\x1b[0m Total Score: 1” - Display the total score as a decimal Arabic numeral.
-	4. The table immediately follows “Total Score” and should be inserted directly without using a code block. Keep any ANSI escape code in the table.
+	2. Change "<RISK_LEVEL>" in "WARNING: <RISK_LEVEL>" to the risk level with the first letter of each word capitalized.
+	3. Change "<TOTAL_SCORE>" in "Total Score: <TOTAL_SCORE>" to the total score, expressed as a decimal numeral.
+	4. Without using a code block, place the output of the tool, GetSummaryReportTable, in the line directly after "Total Score: <TOTAL_SCORE>".
 
-Example:
+The Summary Report Example:
 
-\x1b[1m\x1b[33m[!]\x1b[0m\x1b[0m WARNING: High Risk
-\x1b[1m\x1b[36m[*]\x1b[0m\x1b[0m Total Score: 1
-+------------------------+----------------------------+------------+-------+--------+
-| Filename               | Rule                       | Confidence | Score | Weight |
-+------------------------+----------------------------+------------+-------+--------+
-| writeContentToLog.json | Write contents to the log. | 40%        | 1     | 0.125  |
-+------------------------+----------------------------+------------+-------+--------+
+[!] WARNING: <RISK_LEVEL>
+[*] Total Score: <TOTAL_SCORE>
++--------------------------------+-----------------------------+------------+-------+--------+  
+| Filename                       | Rule                        | Confidence | Score | Weight |  
++--------------------------------+-----------------------------+------------+-------+--------+  
+| constructCryptoGraphicKey.json | Construct cryptographic key | 100%       | 1     | 1.0    |  
++--------------------------------+-----------------------------+------------+-------+--------+ 
 
-Ensure you adhere to this format when generating a summary report.
+Ensure you adhere to these rules and the example when providing a summary report.
 
 """
