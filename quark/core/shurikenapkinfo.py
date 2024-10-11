7ce143ee899ec75fe1e7d871645acf7b7e4df780
zinwang <chriswangxxxxx@gmail.com>
2024-10-11 14:34:33 +0800

    Adjust code style

--- quark/core/shurikenapkinfo.py
+++ quark/core/shurikenapkinfo.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# This file is part of Quark-Engine - https://github.com/quark-engine/quark-engine
+# This file is part of Quark-Engine:
+# https://github.com/quark-engine/quark-engine
 # See the file 'LICENSE' for copying permission.
 
 import re
