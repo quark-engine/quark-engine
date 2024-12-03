b147e9fcdc1a40b70a94c316d21c30fb4219e75a
zinwang <32264884+zinwang@users.noreply.github.com>
2024-12-02 20:34:58 -0500

    Update version information to v24.12.1 (#721)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = 'Quark-Engine Team'
 author = 'KunYu Chen, JunWei Song, YuShiang Dang, ShengFone Lu, AnWei Kung, ShunTe Lin, Vivek, Krishna'
 
 # The full version, including alpha/beta/rc tags
-release = 'v24.11.2'
+release = 'v24.12.1'
 
 # -- General configuration ---------------------------------------------------
 
